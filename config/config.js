10075cc6907a972f52ef80a73024b402bc4c4a09
ryan1002 <ryanmodeste2@gmail.com>
2016-09-16 10:57:16 +0100

    Added monglab to config

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/cinemalist',
+  db: process.env.MONGOLAB_URI || 'mongodb://localhost/cinemalist',
   secret: process.env.SECRET || "gosh this is so secret... shhh..."
 };
