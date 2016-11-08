12c5656d2ef50a70269562fb7bb95d33f61eb78c
ryan1002 <ryanmodeste2@gmail.com>
2016-11-08 14:52:49 +0000

    added MONGODB_URI

--- cinemas/config/config.js
+++ cinemas/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  db: 'mongodb://localhost/express-authentication-jwt',
+  db: process.env.MONGODB_URI || 'mongodb://localhost/express-authentication-jwt',
   secret: process.env.SECRET || "gosh this is so secret... shhh..."
 };
